39f5c40ce85c80114c8e1c1d043b0d5fed147fb7
Rashid <rashidpk090@gmail.com>
2024-08-13 09:53:12 +0530

    asd

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -4,9 +4,9 @@ import {earaids} from '../assets'
 const AboutUs = () => {
   return (
     <section className='w-11/12 xl:w-10/12 mx-auto h-full py-10'>
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 gap-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 gap-10 xl:pt-20'>
             <div className='xl:col-span-2 grid place-items-center'>
-                <img className='h-[300px] w-[300px] xl:h-[500px] xl:w-[500px] object-cover' src={earaids} loading='lazy' alt="safeears" />
+                <img className='h-[300px] w-[300px] xl:h-[400px] xl:w-[400px] object-cover' src={earaids} loading='lazy' alt="safeears" />
             </div>
             <div className='xl:col-span-3 flex items-center'>
                 <p className='text-lg'>At Safe Ears, we understand the profound impact that hear infections
