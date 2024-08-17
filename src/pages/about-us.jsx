299846dc3ef5d6af887571bba5c61f076462de1f
Rashid <rashidpk090@gmail.com>
2024-08-17 10:10:31 +0530

    asd

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -8,14 +8,8 @@ const AboutUs = () => {
             <div className='xl:col-span-2 grid place-items-center'>
                 <img className='h-[300px] w-[300px] xl:h-[400px] xl:w-[400px] object-cover' src={earaids} loading='lazy' alt="safeears" />
             </div>
-            <div className='xl:col-span-3 flex items-center'>
-                <p className='text-lg'>At Safe Ears, we understand the profound impact that hear infections
-                can have on your daily life. That's why we're dedicated to providing
-                top-quality hearing aids designed to enhance your hearing experi-
-                ence and improve your quality of life. Our brand, Safe Ears, stands for
-                reliability, innovation, and exceptional customer care.
-                We have experienced doctors and audiologist in our panel board to
-                analyse the product.</p>
+            <div className='xl:col-span-3 flex items-center '>
+                <p className='text-lg'>SAFE EARS is an innovation which discloses a ear-protection aid after 8years of R&D. It is an auricular shaped product which is used to cover the external ear to prevent water or foreign particles from entering the ear canal. SAFE EARS is made of food-grade plastic material like Polypropylene. Safe Ears is a Patent Applied Product. Safe Ears is a ventured by Grand VF PVT.Ltd</p>
             </div>
         </div>
     </section>
